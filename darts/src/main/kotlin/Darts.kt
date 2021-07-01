54744ee2bb1017f2818e5b3b922756700c8bad53
ThunderFollower <44620256+ThunderFollower@users.noreply.github.com>
2021-07-01 19:03:15 +0300

    Darts fixed (added better solution)

--- darts/src/main/kotlin/Darts.kt
+++ darts/src/main/kotlin/Darts.kt
@@ -1,36 +1,50 @@
+import kotlin.math.hypot
 import kotlin.math.sqrt
 
 object Darts {
-
-    fun score(x: Int , y: Int): Int {
-        val distance = sqrt((x * x + y * y).toDouble())
-        when {
-            distance <= 1  -> return 10
-            distance <= 5  -> return 5
-            distance <= 10 -> return 1
+    //Best solution
+    fun score(x: Number, y: Number): Int {
+        val offset = hypot(x.toDouble(),y.toDouble())
+        return when {
+            offset <= 1  -> 10
+            offset <= 5  -> 5
+            offset <= 10 -> 1
+            else -> 0
         }
-        return 0
     }
 
-    fun score(x: Double , y: Int): Int {
-        val distance = sqrt((x * x + y * y))
-        when {
-            distance <= 1  -> return 10
-            distance <= 5  -> return 5
-            distance <= 10 -> return 1
-        }
-        return 0
-    }
 
-    fun score(x: Double , y: Double): Int {
-        val distance = sqrt((x * x + y * y).toDouble())
-        when {
-            distance <= 1  -> return 10
-            distance <= 5  -> return 5
-            distance <= 10 -> return 1
-        }
-        return 0
-    }
+    //Polymorphism
+
+//    fun score(x: Int , y: Int): Int {
+//        val distance = sqrt((x * x + y * y).toDouble())
+//        when {
+//            distance <= 1  -> return 10
+//            distance <= 5  -> return 5
+//            distance <= 10 -> return 1
+//        }
+//        return 0
+//    }
+//
+//    fun score(x: Double , y: Int): Int {
+//        val distance = sqrt((x * x + y * y))
+//        when {
+//            distance <= 1  -> return 10
+//            distance <= 5  -> return 5
+//            distance <= 10 -> return 1
+//        }
+//        return 0
+//    }
+//
+//    fun score(x: Double , y: Double): Int {
+//        val distance = sqrt((x * x + y * y).toDouble())
+//        when {
+//            distance <= 1  -> return 10
+//            distance <= 5  -> return 5
+//            distance <= 10 -> return 1
+//        }
+//        return 0
+//    }
 }
 
 
